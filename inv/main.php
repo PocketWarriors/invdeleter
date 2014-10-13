cd5b427291e02b393073f08ff1fe9293c19f63e5
Wantedkillers <Wantedkillerss@users.noreply.github.com>
2014-10-13 18:39:03 -0300

    Update main.php

--- inv/main.php
+++ inv/main.php
@@ -28,3 +28,54 @@ use pocketmine\scheduler\PluginTask;
 
 class SkyWars extends PluginBase implements Listener{
 	
+public $isworldswitched = false;
+public $config;
+//public $bplayers;
+//public $cplayers;
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager->registerEvents($this, $this); 
+        	$this->getLogger()->info(TextFormat::DARK_RED . "Inv" . TextFormat::DARK_BLUE . "deleter" . TextFormat::AQUA . "plugin by PocketWarriors is loading...");
+        	$this->config = new Config($this->getDataFolder()."INVDELETER", Config::YAML, array(
+        	"Delete-inventory-on-world-switch" => true,
+          "Allow_op_to_use_/deleteinv_<player>" => true,
+            	$this->config->save();
+            
+	}
+
+	public function onDisable(){
+        	$this->getLogger()->info(TextFormat::GOLD . "INVDELETER plugin by PocketWarriors is disabling...");
+        	$this->config->save();
+        }
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+			case "deleteinv":
+        			if($sender->hasPermission("deleteinv.command") {
+               $check = $this->config->get("Allow_op_to_use_/deleteinv"){
+               $goodtogo = if($sender->isOp and if($this->check = true); 
+               $name = $this->player->getName()->getInv();
+               if($goodtogo = true; $this->player->setInv(0);
+                return true;
+    	          }
+	               else{
+	            $sender->sendMessage("Something went wrong make sure player exists and youre op with permission to do this command.");
+	               }
+	            return false;     
+					           }
+					        }
+				}
+		}
+	}
+	
+	public function onLevelChange(EntityLevelChangeEvent $event){
+    $check1 = $this->config->get("Delete-inventory-on-world-switch" => true,){
+    $goodtogo1 = this->check1 = true;
+    if($goodtogo1 = true; $entity->getName()->getInv();
+    $entity->setInv(0);
+     return true;
+      }else{
+     return false;
+  				}
+		}
+	}
